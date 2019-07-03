33024a2d0202124cbc53f4d106a7fed03bae41f8
Jimmy Chu <jimmychu0807@gmail.com>
2019-07-03 10:37:59 +0800

    competed start_auction code

--- runtime/src/cat_auction.rs
+++ runtime/src/cat_auction.rs
@@ -17,6 +17,8 @@ use runtime_io;
 use runtime_primitives::traits::{ As, CheckedAdd, CheckedDiv, CheckedMul, Hash };
 use parity_codec::{ Encode, Decode };
 
+pub type StdResult<T> = rstd::result::Result<T, &'static str>;
+
 /// The module's configuration trait. This is trait inheritance.
 pub trait Trait: timestamp::Trait + balances::Trait {
   type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -89,8 +91,8 @@ pub struct AuctionTx<Hash, AccountId, Balance, Moment> {
 decl_storage! {
   trait Store for Module<T: Trait> as CatAuction {
     Kitties get(kitties): map T::Hash => Kitty<T::Hash, T::AccountId>;
-    AllKittiesArray get(kitty_array): map u64 => T::Hash;
-    AllKittiesCount get(all_kitties_count): u64 = 0;
+    KittiesArray get(kitty_array): map u64 => T::Hash;
+    KittiesCount get(kitties_count): u64 = 0;
 
     // The following two go hand-in-hand, write to one likely need to update the other two
     OwnerKitties get(owner_kitties): map (T::AccountId, u64) => T::Hash;
@@ -99,8 +101,8 @@ decl_storage! {
     // On Auction
     Auctions get(auctions): map T::Hash => Auction<T::Hash, T::Balance, T::Moment,
       AuctionTx<T::Hash, T::AccountId, T::Balance, T::Moment>>;
-    AllAuctionsArray get(auction_array): map u64 => T::Hash;
-    AllAuctionsCount get(all_auctions_count): u64 = 0;
+    AuctionsArray get(auction_array): map u64 => T::Hash;
+    AuctionsCount get(auctions_count): u64 = 0;
 
     // `bid_id` => Bid object
     Bids get(bids): map T::Hash => Bid<T::Hash, T::AccountId, T::Balance, T::Moment>;
@@ -117,11 +119,13 @@ decl_storage! {
 decl_event!(
   pub enum Event<T> where
     <T as system::Trait>::AccountId,
-    <T as system::Trait>::Hash, {
-    // <T as balances::Trait>::Balance, {
+    <T as system::Trait>::Hash,
+    <T as balances::Trait>::Balance,
+    <T as timestamp::Trait>::Moment {
 
     // Events in our runtime
     KittyCreated(AccountId, Hash, Vec<u8>),
+    AuctionStarted(AccountId, Hash, Hash, Balance, Moment),
   }
 );
 
@@ -132,13 +136,10 @@ decl_module! {
     pub fn create_kitty(origin, kitty_name: Vec<u8>) -> Result {
       let sender = ensure_signed(origin)?;
 
-      // generate a random hash key
-      let nonce = <Nonce<T>>::get();
-      let random_seed = <system::Module<T>>::random_seed();
-      let kitty_id = (random_seed, &sender, nonce).using_encoded(<T as system::Trait>::Hashing::hash);
+      let kitty_id = Self::_gen_random_hash(&sender)?;
 
       // ensure the kitty_id is not existed
-      ensure!(!<Kitties<T>>::exists(kitty_id), "Cat with the id existed already");
+      ensure!(!<Kitties<T>>::exists(&kitty_id), "Cat with the id existed already");
 
       let owner_kitties_count = Self::owner_kitties_count(&sender);
 
@@ -152,19 +153,15 @@ decl_module! {
 
       // update corresponding storage
       <Kitties<T>>::insert(kitty_id, &kitty);
-      <AllKittiesArray<T>>::insert(Self::all_kitties_count(), &kitty_id);
-      <AllKittiesCount<T>>::mutate(|cnt| *cnt += 1);
+      <KittiesArray<T>>::insert(Self::kitties_count(), &kitty_id);
+      <KittiesCount<T>>::mutate(|cnt| *cnt += 1);
 
       // update OwnerKitties...
       <OwnerKitties<T>>::insert((sender.clone(), owner_kitties_count), &kitty_id);
       <OwnerKittiesCount<T>>::mutate(&sender, |cnt| *cnt += 1);
 
-      // nonce increment by 1
-      <Nonce<T>>::mutate(|nonce| *nonce += 1);
-
       // emit an event
       Self::deposit_event(RawEvent::KittyCreated(sender, kitty_id, kitty_name));
-
       Ok(())
     } // end of fn `create_kitty`
 
@@ -181,6 +178,7 @@ decl_module! {
       // check #1
       ensure!(<Kitties<T>>::exists(kitty_id), "Kitty does not exist");
       let kitty = Self::kitties(kitty_id);
+
       // check #2
       ensure!(!kitty.in_auction, "Kitty is already in another auction");
 
@@ -194,99 +192,47 @@ decl_module! {
 
       // Write:
       //  1. create the auction
+      let auction_id = Self::_gen_random_hash(&sender)?;
+
+      // check: auction_id not existed yet
+      ensure!(!<Auctions<T>>::exists(&auction_id), "Auction ID generated exists already");
+
+      let auction = Auction {
+        id: auction_id.clone(),
+        kitty_id,
+        base_price,
+        start_time: now,
+        end_time: end_time.clone(),
+        status: AuctionStatus::Ongoing,
+        tx: None,
+      };
+
+      <Auctions<T>>::insert(auction_id, auction);
+      <AuctionsArray<T>>::insert(Self::auctions_count(), auction_id);
+      <AuctionsCount<T>>::mutate(|cnt| *cnt += 1);
+
       //  2. set the kitty state in_auction = true
+      <Kitties<T>>::mutate(kitty_id, |k| k.in_auction = true);
 
-
+      // emit an event
+      Self::deposit_event(RawEvent::AuctionStarted(sender, kitty_id, auction_id, base_price, end_time));
       Ok(())
-    }
+    } // end of `fn start_auction(...)
 
-    // pub fn for_sale(origin, kitty_id: T::Hash, base_price: u64) -> Result {
-    //   let sender = ensure_signed(origin)?;
+  } // end of `struct Module<T: Trait> for enum Call...`
 
-    //   // 1. check the origin own the kitty
-    //   // 2. check the price is > 0
-    //   let kitty_owner = Self::owner_of(kitty_id).ok_or("Kitty has no owner")?;
-    //   ensure!(kitty_owner == sender, "The cat is not owned by the requester.");
-    //   ensure!(base_price > 0, "The price must be set higher than 0.");
+} // end of `decl_module!`
 
-    //   // 3. set the base price of the kitty, write to blockchain
-    //   <Kitties<T>>::mutate(kitty_id, |kitty| {
-    //     kitty.base_price = T::Balance::sa(base_price);
-    //   });
+impl<T: Trait> Module<T> {
+  // generate a random hash key
+  fn _gen_random_hash(sender: &T::AccountId) -> StdResult<T::Hash> {
+    let nonce = <Nonce<T>>::get();
+    let random_seed = <system::Module<T>>::random_seed();
+    let random_hash = (random_seed, sender, nonce).using_encoded(<T as system::Trait>::Hashing::hash);
 
-    //   // 4. emit an event
-    //   Self::deposit_event(RawEvent::ForSale(kitty_id, base_price));
+    // nonce increment by 1
+    <Nonce<T>>::mutate(|nonce| *nonce += 1);
 
-    //   // 5. return
-    //   Ok(())
-    // }
-
-    // pub fn cancel_sale(origin, kitty_id: T::Hash) -> Result {
-    //   let sender = ensure_signed(origin)?;
-
-    //   // 1. check the origin own the kitty
-    //   // 2. check the price is > 0
-    //   let kitty_owner = Self::owner_of(kitty_id).ok_or("Kitty has no owner")?;
-    //   ensure!(kitty_owner == sender, "The cat is not owned by the requester.");
-
-    //   // 3. set the base price of the kitty to 0
-    //   <Kitties<T>>::mutate(kitty_id, |kitty| {
-    //     kitty.base_price = T::Balance::sa(0);
-    //   });
-
-    //   Ok(())
-    // }
-
-    // pub fn transaction(origin, kitty_id: T::Hash) -> Result {
-    //   let sender = ensure_signed(origin)?;
-
-    //   // 1. get the kitty
-    //   //   - if the kitty has no owner, throw error
-    //   //   - check the user at least kitty.base_price in his balance
-    //   let kitty_owner = Self::owner_of(kitty_id).ok_or("Kitty has no owner.")?;
-    //   let mut kitty = Self::kitties(kitty_id);
-    //   let transaction_price = kitty.base_price;
-
-    //   ensure!(transaction_price.as_() > 0, "This kitty is not for sale.");
-    //   ensure!(sender != kitty_owner, "You cannot purchase from your own");
-    //   ensure!(<balances::Module<T>>::free_balance(&sender) >= transaction_price, "You don't have enough balance to purchase this kitty.");
-
-    //   // 2. do the exchange
-    //   //   - sender balance is deducted, kitty owner balance is incremented
-    //   <balances::Module<T> as Currency<_>>::transfer(&sender, &kitty_owner, transaction_price)?;
-
-    //   //   - the kitty is changed hand, update all relevant data structure
-    //   // a. update kitty with `base_price` set to 0,
-    //   kitty.base_price = T::Balance::sa(0);
-    //   <Kitties<T>>::insert(kitty.id, kitty);
-
-    //   // b. update kitty owner storage
-    //   <KittyOwner<T>>::insert(kitty.id, &sender);
-
-    //   // c. update ownedKitties storage,
-    //   //   c1. remove from original owner
-    //   <OwnedKitties<T>>::mutate(kitty_owner, |owned_vec| {
-    //     // ENHANCE: find a better way of writing this:
-    //     let kitty_index = 0;
-    //     for (i, el) in owned_vec.iter().enumerate() {
-    //       if el.as_bytes() == kitty.id.as_bytes() {
-    //         kitty_index = i;
-    //         break;
-    //       }
-    //     }
-    //     owned_vec.remove(kitty_index);
-    //   });
-
-    //   //   c2. add to the new owner
-    //   <OwnedKitties<T>>::mutate(sender, |owned_vec| {
-    //     owned_vec.push(kitty.id)
-    //   });
-
-    //   // 3. emit an event
-    //   Self::deposit_event(RawEvent::Transaction(kitty_owner, sender, kitty_id, transaction_price.as_()));
-
-    //   // 4. return
-    //   Ok(())
-    // } // end of `transaction`
+    Ok(random_hash)
   }
 }
